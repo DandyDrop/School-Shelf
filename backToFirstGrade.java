55e6d5107bdf9466417a209aebcf00bd7085f810
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-02-08 16:45:48 +0200

    Update backToFirstGrade.java

--- backToFirstGrade.java
+++ backToFirstGrade.java
@@ -10,9 +10,9 @@ public class zonina {
   public static void main(String[] args) {
     for (int i = 1; i < 10; i++) {
       System.out.println(i);
-      for (int j = 2 j < i + 1; i++) {
-        
-      }
+      int p = 1;
+      for (int j = 2 j < i + 1; i++) p = p * j;
+      System.out.println(i+ " : " +p)
     }
   }
 }

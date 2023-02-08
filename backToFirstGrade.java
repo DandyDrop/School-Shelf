2b902da1843aae982cd2bf280ed828c07dee5884
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-02-08 16:18:29 +0200

    Create backToFirstGrade.java

---
+++ backToFirstGrade.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class backing {
+  public static void main(String[] args) {
+    for (int i = 100; i < 1000; i++) if ((i % 10) + (i / 100) == (i/10%10)) System.out.println(i);
+  }
+}
+
+public class zonina {
+  public static void main(String[] args) {
+    for (int i = 1; i < 10; i++) {
+      System.out.println(i);
+      for (int j = 2 j < i + 1; i++) {
+        
+      }
+    }
+  }
+}
+
+
